d5228bb37d41b389d6abb228dcbf70f177eef28e
Gwinbleind <borislav31@ya.ru>
2019-09-09 14:13:13 +0300

    Корзина
    Взял сверстанный сайт, в него формирую корзину

--- lesson_5.js
+++ lesson_5.js
@@ -1,3 +1,4 @@
+// Задание 1, создание шахматной доски
 const desk = document.createElement('div');
 const blackCell = desk.cloneNode(true);
 const whiteCell = blackCell.cloneNode(true);
@@ -15,8 +16,6 @@ let str = ' 12345678';
 const rowName = [...str];
 str = ' ABCDEFGH';
 const colName = [...str];
-const h2Tag = document.getElementsByTagName('h2')[0];
-h2Tag.insertAdjacentElement('afterend',desk);
 
 function blackWhiteReturn(f) {
     if (f) {
@@ -27,6 +26,8 @@ function blackWhiteReturn(f) {
 }
 
 function generateDesk() {
+    const h2Tag = document.getElementsByTagName('h2')[0];
+    h2Tag.insertAdjacentElement('afterend',desk);
     let f = true;
     rowName.forEach(rowElem => {
         colName.forEach(colElem => {
@@ -45,7 +46,58 @@ function generateDesk() {
             desk.insertAdjacentElement('beforeend',s);
         });
         f = !f
-    })
+    });
+    document.querySelector('input#point1').setAttribute('disabled', 'disabled'); //Кнопка выключается
 }
 
-generateDesk();
+//Задание 3 - создание каталога
+const sampleProduct = {
+    id_product: 456,
+    product_name: "Мышка",
+    price: 1000,
+    quantity: 1
+};
+const product = [sampleProduct];
+let elem = Object.create(sampleProduct);
+elem.id_product++;
+elem.product_name = 'Кошка';
+elem.price *= 2;
+product.push(elem);
+elem = Object.create(sampleProduct);
+elem.id_product += 2;
+elem.product_name = 'Собака';
+elem.price *= 3;
+product.push(elem);
+
+//Задание 2 - создание корзины
+const cart = {
+        amount: 0,
+        countGoods: 0,
+        content: [],
+    emptyCart() {
+        this.amount = 0;
+        this.countGoods = 0;
+        this.content = []
+    },
+    sampleCart() {
+        cart.content.push(sampleProduct);
+        let elem = Object.create(sampleProduct);
+        elem.id_product++;
+        elem.product_name = 'Кошка';
+        elem.price *= 2;
+        this.push(elem);
+        elem = Object.create(sampleProduct);
+        elem.id_product += 2;
+        elem.product_name = 'Собака';
+        elem.price *= 3;
+        this.push(elem);
+    },
+    countCart() {
+            this.countGoods = 0;
+            this.amount = 0;
+            this.content.forEach(element => {
+              this.amount += element.price * element.quantity;
+              this.countGoods += element.quantity;
+            })
+    }
+};
