93792779ccb2c26d65fab1f91c96cd09557bcc7b
Gwinbleind <borislav31@ya.ru>
2019-09-23 12:55:17 +0300

    Update

--- cart.js
+++ cart.js
@@ -1,4 +1,39 @@
 cartBoxTag = document.querySelector('.drop__cart');
-cart = {
 
+const cart = {
+    amount: 0,
+    countGoods: 0,
+    content: [],
+    emptyCart() {
+        this.amount = 0;
+        this.countGoods = 0;
+        this.content = []
+    },
+    sampleCart() {
+        this.emptyCart();
+        cart.content.push(sampleProduct);
+        let elem = Object.create(sampleProduct);
+        elem.id_product++;
+        elem.product_name = 'Кошка';
+        elem.price *= 2;
+        elem.quantity = 2;
+        this.content.push(elem);
+        elem = Object.create(sampleProduct);
+        elem.id_product += 2;
+        elem.product_name = 'Собака';
+        elem.price *= 3;
+        elem.quantity = 3;
+        this.content.push(elem);
+    },
+    countCart() {
+        this.countGoods = 0;
+        this.amount = 0;
+        this.content.forEach(element => {
+            this.amount += element.price * element.quantity;
+            this.countGoods += element.quantity;
+        })
+    },
+    renderDropCart () {
+
+    },
 };
